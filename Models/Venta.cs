031f4c6167e0ae10501afae4f536c59b74d63389
Alberto-Rojas96 <105465978+Alberto-Rojas96@users.noreply.github.com>
2022-05-12 18:47:49 -0500

    Initial Commit

---
+++ Models/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WSVenta.Models
+{
+    public partial class Venta
+    {
+        public long Id { get; set; }
+        public int? IdCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal? Total { get; set; }
+
+        public virtual Cliente IdClienteNavigation { get; set; }
+        public virtual Concepto IdNavigation { get; set; }
+    }
+}
